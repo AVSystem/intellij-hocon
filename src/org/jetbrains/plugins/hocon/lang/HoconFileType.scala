3e815dd2d7aa0ecdacaaabc0fde15ce9236bb8cf
ghik <romeqjanoosh@gmail.com>
2019-02-21 13:31:29 +0100

    simplifying HOCON language description

--- src/org/jetbrains/plugins/hocon/lang/HoconFileType.scala
+++ src/org/jetbrains/plugins/hocon/lang/HoconFileType.scala
@@ -2,16 +2,13 @@ package org.jetbrains.plugins.hocon.lang
 
 import com.intellij.icons.AllIcons
 import com.intellij.openapi.fileTypes.LanguageFileType
+import javax.swing.Icon
 
 object HoconFileType extends LanguageFileType(HoconLanguage) {
   val DefaultExtension = "conf"
 
-  def getIcon = AllIcons.FileTypes.Config
-
-  def getDefaultExtension = DefaultExtension
-
-  def getDescription = "Human-Optimized Config Object Notation"
-
+  def getIcon: Icon = AllIcons.FileTypes.Config
+  def getDefaultExtension: String = DefaultExtension
+  def getDescription = "HOCON"
   def getName = "HOCON"
-
 }
