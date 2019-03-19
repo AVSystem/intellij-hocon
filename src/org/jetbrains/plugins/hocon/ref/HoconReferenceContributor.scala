ab909606312f2f8194da51d5aaa546d0a99d5ecf
Mikhail Mutcianko <mutcianko.m@gmail.com>
2019-03-19 21:09:06 +0300

    proper way of loading java specific support fix #2

--- src/org/jetbrains/plugins/hocon/ref/HoconReferenceContributor.scala
+++ src/org/jetbrains/plugins/hocon/ref/HoconReferenceContributor.scala
@@ -1,18 +1,17 @@
 package org.jetbrains.plugins.hocon.ref
 
-import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.{PlatformPatterns, PsiElementPattern}
 import com.intellij.psi.{PsiReferenceContributor, PsiReferenceRegistrar}
 import org.jetbrains.plugins.hocon.psi.{HQualifiedIncluded, HString}
 
 class HoconReferenceContributor extends PsiReferenceContributor {
+  import HoconReferenceContributor._
+
   def registerReferenceProviders(registrar: PsiReferenceRegistrar): Unit = {
-    val hStringPattern = PlatformPatterns.psiElement(classOf[HString])
-    try {
-      getClass.getClassLoader.loadClass("com.intellij.psi.impl.source.resolve.reference.impl.providers.JavaClassReferenceProvider")
-      registrar.registerReferenceProvider(hStringPattern, new HStringJavaClassReferenceProvider)
-    } catch {
-      case _: ClassNotFoundException => // no java support, skip java reference provider
-    }
     registrar.registerReferenceProvider(hStringPattern.withParent(classOf[HQualifiedIncluded]), new IncludedFileReferenceProvider)
   }
+}
+
+object HoconReferenceContributor {
+  val hStringPattern: PsiElementPattern.Capture[HString] = PlatformPatterns.psiElement(classOf[HString])
 }
