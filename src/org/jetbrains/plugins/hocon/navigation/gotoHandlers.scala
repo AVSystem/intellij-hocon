2c18dbc280b23a9ce7fe1e9ce3e62a1b57ec7817
ghik <romeqjanoosh@gmail.com>
2019-08-07 17:17:57 +0200

    better resolution of substitution subpaths

--- src/org/jetbrains/plugins/hocon/navigation/gotoHandlers.scala
+++ src/org/jetbrains/plugins/hocon/navigation/gotoHandlers.scala
@@ -10,11 +10,23 @@ import org.jetbrains.plugins.hocon.CommonUtil._
 import org.jetbrains.plugins.hocon.psi.{HKeyedField, HPath, ResolutionCtx}
 
 class HoconGotoDeclarationHandler extends GotoDeclarationHandler {
-  def getGotoDeclarationTargets(sourceElement: PsiElement, offset: Int, editor: Editor): Array[PsiElement] =
+  def getGotoDeclarationTargets(sourceElement: PsiElement, offset: Int, editor: Editor): Array[PsiElement] = {
     sourceElement.parentOfType[HPath].fold(PsiElement.EMPTY_ARRAY) { path =>
       val resCtx = ResolutionCtx(path.getContainingFile, followIncludes = true)
-      path.resolve(reverse = true, resCtx).nextOption.toArray
+      val fullPathResolved = for {
+        fullPath <- path.substitution.path
+        keyedField <- fullPath.resolve(reverse = true, resCtx).nextOption
+      } yield (fullPath, keyedField)
+
+      def gotoPrefix(res: Option[(HPath, HKeyedField)]): Option[HKeyedField] = res match {
+        case Some((`path`, field)) => Some(field)
+        case Some((subpath, field)) =>
+          gotoPrefix(for (pref <- subpath.prefix; prefField <- field.prefixingField) yield (pref, prefField))
+        case _ => None
+      }
+      gotoPrefix(fullPathResolved).toArray[PsiElement]
     }
+  }
 }
 
 class HoconGotoSuperHandler extends CodeInsightActionHandler {
