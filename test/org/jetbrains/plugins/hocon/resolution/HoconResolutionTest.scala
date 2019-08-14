d6dda48899b58c4ea8ecb10325a103c884330a3b
ghik <romeqjanoosh@gmail.com>
2019-08-14 16:34:28 +0200

    some more resolution tests

--- test/org/jetbrains/plugins/hocon/resolution/HoconResolutionTest.scala
+++ test/org/jetbrains/plugins/hocon/resolution/HoconResolutionTest.scala
@@ -7,33 +7,60 @@ import org.junit.Assert
 class HoconResolutionTest extends HoconSingleModuleTest {
   def rootPath = s"$testdataPath/hocon/resolution"
 
-  private def render(occurences: Iterator[ResolvedField]): String =
-    occurences.map(rf => s"${rf.field.hoconFile.getName}:${rf.field.pos}").mkString("\n")
-
   private def testPath(file: String, hoconPath: String, expected: String): Unit = {
-    val expectedSplit = expected.split("\n").iterator.map(_.trim).filter(_.nonEmpty).toList
     val hoconFile = findHoconFile(file, project)
     val ctx = ToplevelCtx(hoconFile, ToplevelCtx.referenceFilesFor(hoconFile))
     val path = hoconPath.split('.').toList
 
     def test(reverse: Boolean): Unit = {
-      val opts = ResOpts(reverse)
-      val expectedStr = (if (reverse) expectedSplit.reverse else expectedSplit).mkString("\n")
+      def render(occurences: Iterator[ResolvedField]): String = {
+        val rendered = occurences.map(rf => s"${rf.field.hoconFile.getName}:${rf.field.pos}")
+        (if (reverse) rendered.toList.reverseIterator else rendered).mkString("", "\n", "\n")
+      }
 
+      val opts = ResOpts(reverse)
       val actualResult = render(ctx.occurrences(path, opts))
-      Assert.assertEquals(expectedStr, actualResult)
+      Assert.assertEquals(expected, actualResult)
 
       val traversalResult = render(Iterator.iterate(ctx.occurrences(path, opts).nextOption.orNull) { rf =>
         rf.nextOccurrence(opts).orNull
       }.takeWhile(_ != null))
-      Assert.assertEquals(expectedStr, traversalResult)
+      Assert.assertEquals(expected, traversalResult)
     }
 
     test(reverse = true)
     test(reverse = false)
   }
 
-  def testSimpleResolution(): Unit = testPath("application.conf", "a.b.c",
+  def testSingleKey(): Unit = testPath("application.conf", "a",
+    """reference.conf:1:0
+      |application.conf:1:0
+      |application.conf:3:0
+      |application.conf:8:0
+      |included.conf:1:0
+      |moreIncluded.conf:1:0
+      |included.conf:3:0
+      |application.conf:18:0
+      |application.conf:24:0
+      |""".stripMargin
+  )
+
+  def testMidPath(): Unit = testPath("application.conf", "a.b",
+    """reference.conf:1:2
+      |application.conf:1:2
+      |application.conf:4:2
+      |application.conf:5:2
+      |application.conf:8:2
+      |included.conf:1:2
+      |moreIncluded.conf:1:2
+      |included.conf:3:2
+      |nestedIncluded.conf:1:0
+      |application.conf:21:2
+      |application.conf:24:2
+      |""".stripMargin
+  )
+
+  def testFullPath(): Unit = testPath("application.conf", "a.b.c",
     """reference.conf:1:4
       |application.conf:1:4
       |application.conf:4:4
