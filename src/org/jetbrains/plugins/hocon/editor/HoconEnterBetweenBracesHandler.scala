b05475644be00d9cd2950bbf833ee5792acf7a5c
Bartłomiej Kozak <b.kozak@avsystem.com>
2026-03-05 12:45:00 +0100

    Migrate `HoconEnterBetweenBracesHandler` to use `EnterBetweenBracesDelegate` API and update `plugin.xml` accordingly.

--- src/org/jetbrains/plugins/hocon/editor/HoconEnterBetweenBracesHandler.scala
+++ src/org/jetbrains/plugins/hocon/editor/HoconEnterBetweenBracesHandler.scala
@@ -1,37 +1,9 @@
 package org.jetbrains.plugins.hocon
 package editor
 
-import com.intellij.codeInsight.editorActions.enter.EnterHandlerDelegate.Result
-import com.intellij.codeInsight.editorActions.enter.{EnterBetweenBracesHandler, EnterHandlerDelegate}
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.actionSystem.EditorActionHandler
-import com.intellij.openapi.util.Ref
-import com.intellij.psi.PsiFile
-import org.jetbrains.plugins.hocon.lang.HoconLanguage
+import com.intellij.codeInsight.editorActions.enter.EnterBetweenBracesDelegate
 
-import scala.annotation.nowarn
-
-/** Like com.intellij.json.formatter.JsonEnterBetweenBracesHandler
-  *
-  * @author
-  *   ghik
-  */
-@nowarn("msg=deprecated")
-class HoconEnterBetweenBracesHandler extends EnterBetweenBracesHandler {
-  override def preprocessEnter(
-    file: PsiFile,
-    editor: Editor,
-    caretOffsetRef: Ref[Integer],
-    caretAdvance: Ref[Integer],
-    dataContext: DataContext,
-    originalHandler: EditorActionHandler,
-  ): Result =
-    if (file.getLanguage.is(HoconLanguage))
-      super.preprocessEnter(file, editor, caretOffsetRef, caretAdvance, dataContext, originalHandler)
-    else
-      EnterHandlerDelegate.Result.Continue
-
-  override def isBracePair(c1: Char, c2: Char): Boolean =
+class HoconEnterBetweenBracesHandler extends EnterBetweenBracesDelegate {
+  override protected def isBracePair(c1: Char, c2: Char): Boolean =
     c1 == '{' && c2 == '}' || c1 == '[' && c2 == ']'
 }
